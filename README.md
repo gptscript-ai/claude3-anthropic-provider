322ca86f8dffd3dc8c9f4c450b4c28819a7ee829
Taylor Price <tayworm@gmail.com>
2024-05-22 12:37:24 -0700

    chore!: split aws and anthropic serving platforms into separate providers
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-* You need either ANTHROPIC_API_KEY or AWS credentials and region configured, depending on which model you want to use.
-
-```
-export ANTHROPIC_API_KEY=<your-api-key>
-```
+# Claude3 Anthropic Provider
 
 ## Usage Example
 
 ```
-gptscript --default-model='claude-3-haiku-20240307 from github.com/gptscript-ai/anthropic-provider' examples/helloworld.gpt
+gptscript --default-model='claude-3-haiku-20240307 from github.com/gptscript-ai/claude3-anthropic-provider' examples/helloworld.gpt
 ```
 
+The Claude3 Bedrock provider can be found [here](https://github.com/gptscript-ai/claude3-bedrock-provider)
+
 ## Development
+
+* You need an ANTHROPIC_API_KEY set in your environment.
+
+```
+export ANTHROPIC_API_KEY=<your-api-key>
+```
 
 Run using the following commands
 
 ```
 python -m venv .venv
 source ./.venv/bin/activate
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 ./run.sh
 ```
 
@@ -25,9 +29,5 @@ pip install -r requirements.txt
 export OPENAI_BASE_URL=http://127.0.0.1:8000/v1
 export GPTSCRIPT_DEBUG=true
 
-# one of the anthropic-hosted models
 gptscript --default-model=claude-3-opus-20240229 examples/bob.gpt
-
-# Claude on Bedrock
-gptscript --default-model=anthropic.claude-3-sonnet-20240229-v1:0 examples/bob.gpt
 ```
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
-import re
 
-import xmltodict
-from anthropic import AsyncAnthropic, AsyncAnthropicBedrock
+import claudelib
+from anthropic import AsyncAnthropic
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, StreamingResponse
 
-from prompt_constructors import *
-
 debug = os.environ.get("GPTSCRIPT_DEBUG", "false") == "true"
+client = AsyncAnthropic()
 app = FastAPI()
 
 
@@ -34,224 +32,16 @@ async def get_root():
     return 'ok'
 
 
-# The anthropic API does not have a method to list models, so we are hard coding the models here
 @app.get("/v1/models")
 async def list_models() -> JSONResponse:
-    return JSONResponse(content={"data": [
-        {"id": "claude-3-opus-20240229", "name": "Anthropic Claude 3 Opus"},
-        {"id": "claude-3-sonnet-20240229", "name": "Anthropic Claude 3 Sonnet"},
-        {"id": "claude-3-haiku-20240307", "name": "Anthropic Claude 3 Haiku"},
-        {"id": "anthropic.claude-3-opus-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Opus"},
-        {"id": "anthropic.claude-3-sonnet-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Sonnet"},
-        {"id": "anthropic.claude-3-haiku-20240307-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Haiku"},
-    ]})
-
-
-def map_req(req: dict) -> dict:
-    system: str | None = ""
-    mapped_messages: list[dict] = []
-
-    max_tokens = 4096
-    if 'max_tokens' in req.keys():
-        max_tokens = req["max_tokens"]
-
-    if "tools" in req.keys():
-        system += construct_tool_use_system_prompt(req["tools"])
-
-    messages = req["messages"]
-
-    tool_inputs_xml: list[str] = []
-    tool_outputs_xml: list[str] = []
-    for message in messages:
-        if 'role' in message.keys() and message["role"] == "system":
-            system += message["content"] + "\n"
-
-        if 'role' in message.keys() and message["role"] == "user":
-            mapped_messages.append({
-                "role": "user",
-                "content": [
-                    {
-                        "text": message["content"],
-                        "type": "text"
-                    },
-                ]
-            })
-
-        if 'role' in message.keys() and message["role"] == "tool":
-            content: str = '\n' + construct_tool_outputs_message([message], None)
-            mapped_messages.append({
-                "role": "user",
-                "content": content,
-            })
-
-        if 'role' in message.keys() and message["role"] == "assistant":
-            if 'tool_calls' in message.keys():
-                tool_inputs = []
-                for tool_call in message["tool_calls"]:
-                    tool_inputs.append({
-                        "tool_name": tool_call["function"]["name"],
-                        "tool_arguments": tool_call["function"]["arguments"],
-                    })
-                content: str = '\n' + construct_tool_inputs_message(message.get("content", ""), tool_inputs)
-                mapped_messages.append({
-                    "role": "assistant",
-                    "content": content,
-                })
-            else:
-                mapped_messages.append({
-                    "role": "assistant",
-                    "content": message["content"]
-                })
-
-    for message in mapped_messages:
-        if 'role' in message.keys() and message["role"] == "":
-            message["role"] = "assistant"
-
-    mapped_messages = merge_consecutive_dicts_with_same_value(mapped_messages, "role")
-
-    mapped_req = {
-        "messages": mapped_messages,
-        "max_tokens": max_tokens,
-        "system": system,
-        "model": req["model"],
-        "temperature": req["temperature"],
-    }
-    return mapped_req
-
-
-def merge_consecutive_dicts_with_same_value(list_of_dicts, key) -> list[dict]:
-    merged_list = []
-    index = 0
-    while index < len(list_of_dicts):
-        current_dict = list_of_dicts[index]
-        value_to_match = current_dict.get(key)
-        compared_index = index + 1
-        while compared_index < len(list_of_dicts) and list_of_dicts[compared_index].get(key) == value_to_match:
-            list_of_dicts[compared_index]["content"] = current_dict["content"] + (list_of_dicts[compared_index][
-                "content"])
-            current_dict.update(list_of_dicts[compared_index])
-            compared_index += 1
-        merged_list.append(current_dict)
-        index = compared_index
-    return merged_list
+    return await claudelib.list_models(client)
 
 
 @app.post("/v1/chat/completions")
 async def completions(request: Request) -> StreamingResponse:
     data = await request.body()
-    req = map_req(json.loads(data))
-    log("MAPPED REQUEST: ", req)
-
-    if req["model"].startswith("anthropic."):
-        client = AsyncAnthropicBedrock()
-    else:
-        if "ANTHROPIC_API_KEY" not in os.environ:
-            error_message = {
-                "error": "ANTHROPIC_API_KEY environment variable not set"
-            }
-            return StreamingResponse(json.dumps(error_message),
-                                     media_type="application/x-ndjson", status_code=401)
-        client = AsyncAnthropic()
-
-    try:
-        async with client.messages.stream(
-                max_tokens=req["max_tokens"],
-                system=req["system"],
-                messages=req["messages"],
-                model=req["model"],
-                temperature=req["temperature"],
-                stop_sequences=["</function_calls>"],
-        ) as stream:
-            accumulated = await stream.get_final_message()
-
-    except Exception as e:
-        try:
-            error_code = e.__dict__["status_code"]
-        except:
-            error_code = 500
-
-        error_message = {"error from remote": str(e)}
-        return StreamingResponse(json.dumps(error_message), media_type="application/x-ndjson", status_code=error_code)
-
-    resp = "data: " + map_resp(accumulated.json()) + "\n\n"
-    return StreamingResponse(resp, media_type="application/x-ndjson")
-
-
-def map_resp(response) -> str:
-    data = json.loads(response)
-    finish_reason = None
-    parsed_tool_calls = []
-
-    log("INITIAL RESPONSE DATA: ", data)
-
-    for message in data["content"]:
-        if 'text' in message.keys() and "<function_calls>" in message["text"]:
-            pattern = re.compile(r'(<function_calls>.*?</invoke>)', re.DOTALL)
-            match = pattern.search(message["text"])
-            xml_tool_calls = match.group(1) + "</function_calls>"
-            tool_calls = xmltodict.parse(xml_tool_calls)
-            if tool_calls["function_calls"]["invoke"] is list:
-                for key, value in tool_calls["function_calls"]["invoke"].items():
-                    parsed_tool_calls.append({
-                        "index": 0,
-                        "id": value['tool_name'],
-                        "type": "function",
-                        "function": {
-                            "name": value["tool_name"],
-                            "arguments": str(value["parameters"]),
-                        },
-                    })
-            else:
-                parsed_tool_calls.append({
-                    "index": 0,
-                    "id": tool_calls["function_calls"]["invoke"]["tool_name"],
-                    "type": "function",
-                    "function": {
-                        "name": tool_calls["function_calls"]["invoke"]["tool_name"],
-                        "arguments": json.dumps(tool_calls["function_calls"]["invoke"]["parameters"]),
-                    },
-                })
-
-            message.pop("text", None)
-            message.pop("type", None)
-            message["tool_calls"] = parsed_tool_calls
-            message["content"] = None
-            message["role"] = "assistant"
-
-        if 'text' in message.keys():
-            message["content"] = message["text"]
-            message.pop("text", None)
-            message.pop("type", None)
-
-    if "stop_reason" in data.keys() and data["stop_reason"] == "stop_sequence":
-        finish_reason = "tool_calls"
-
-    if "stop_reason" in data.keys() and data["stop_reason"] == "end_turn":
-        finish_reason = "stop"
-
-    log("MAPPED RESPONSE DATA: ", data)
-
-    try:
-        delta = data["content"][0]
-    except:
-        delta = []
-
-    translated = {
-        "id": data["id"],
-        "object": "chat.completion.chunk",
-        "created": 0,
-        "model": data["model"],
-        "system_fingerprint": "TEMP",
-        "choices": [
-            {
-                "index": 0,
-                "delta": delta,
-                "finish_reason": finish_reason,
-            },
-        ],
-    }
-
-    return json.dumps(translated)
+    input = json.loads(data)
+    return await claudelib.completions(client, input)
 
 
 if __name__ == "__main__":
